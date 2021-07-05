cc3da3ec6037b16997d2ae5ca47ada144157e353
Yukesh M <mlyukesh@gmail.com>
2021-07-05 21:50:16 +0530

    first commit

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName, lastName;
+            Console.WriteLine("enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("hi {0} {1}", firstName, lastName);
+        }
+    }
+}
