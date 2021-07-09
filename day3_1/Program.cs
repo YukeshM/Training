6cf4eb8f780fd274b9262a8e2288687b94d5a9e9
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-07-09 08:40:17 +0530

    Add files via upload
    day3_program1

---
+++ day3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace day3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[15];
+            int[] repeatedNumbers = new int[1];
+            Console.WriteLine("Enter how many values do you want to store in a array: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"enter {count}  values: "); 
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] =Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0; i<count; i++)
+            {
+                for(int j = i+1; j < count; j++)
+                {
+                    if (numbers[i] == numbers[j] )
+                        Console.WriteLine("Repeated values are: {0}", numbers[j]);
+                }
+            }
+        }
+
+
+    }
+ }
+
