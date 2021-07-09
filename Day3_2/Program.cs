1768804d4129deabfbc40048330014aed212976d
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-07-09 08:41:23 +0530

    day3_program2

---
+++ Day3_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day3_2
+{
+    class Customer
+    {
+        string customerName;
+        int customerId;
+        public void PlaceOrder()
+        {
+            Console.WriteLine("I just got executed from PlaceOrder.");
+        }
+        static void GetCustomerTimeZone()
+        {
+            Console.WriteLine("I just got executed from GetCustomerTimeZone.");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.customerName = "Ben Tennyson";
+            customer.customerId = 10;
+            customer.PlaceOrder();
+            //customer.GetCustomerTimeZone();
+            var name = customer.customerName;
+            var id = customer.customerId;
+            customer.customerName = "Gwen";
+            Console.WriteLine($"customer name is: {customer.customerName}");
+            Console.WriteLine($"customer id is: {customer.customerId}");
+        }
+    }
+}
