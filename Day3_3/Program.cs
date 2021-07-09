7e934867d050b4a7556d07435106dd05cfb4c61d
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-07-09 08:42:51 +0530

    day3_program3

---
+++ Day3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryName, stateName;
+            Console.WriteLine("Enter country name: ");
+            countryName =Console.ReadLine();
+            Console.WriteLine("Enter state name: ");
+            stateName = Console.ReadLine();
+            countryName = countryName.ToUpper();
+            stateName = stateName.ToUpper();
+            var result = (countryName == "INDIA" && stateName == "KERALA") ? "true" : "false";
+            Console.WriteLine(result);
+        }
+    }
+}
