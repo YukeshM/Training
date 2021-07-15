992b77e7bb634dc7d1a7ec654161626fbca84514
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-07-15 07:02:34 +0530

    stopWatch

---
+++ 14-july-2021/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a program to create a stopwatch in c# 
+
+
+using System;
+using System.Threading;
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.Start();
+            stopWatch.Stop();
+        }
+    }
+}
---
+++ 14-july-2021/StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Day8
+{
+    public class StopWatch
+    {
+
+        public void Start()
+        {
+            if (running)
+            {
+                throw new InvalidOperationException();
+            }
+            running = true;
+            start = DateTime.Now;
+            Console.WriteLine("Start time: " + start.Hour + ":" + start.Minute + ":" + start.Second + ":" + start.Millisecond);
+
+        }
+        public void Stop()
+        {
+            Thread.Sleep(1000);
+            stop = DateTime.Now;
+            running = false;
+            Console.WriteLine("Stop time: " + stop.Hour + ":" + stop.Minute + ":" + stop.Second + ":" + stop.Millisecond);
+            TimeSpan duration = stop - start;
+            Interval(duration);
+
+        }
+        public void Interval(TimeSpan duration)
+        {
+            Console.WriteLine("Interval:" + duration.Seconds + " " + "sec" + duration.Milliseconds + " " + "milliseconds");
+        }
+        public StopWatch()
+        {
+            start = new DateTime();
+            stop = new DateTime();
+            store = new Random();
+            running = false;
+
+        }
+        public DateTime start;
+        public DateTime stop;
+        public Random store;
+        public bool running;
+
+    }
+}
