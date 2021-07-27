3e8f90792f89e420e4c47f09023b46a2c2096ce5
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-07-27 08:01:04 +0530

    26-july

---
+++ 26-july-2021/fiboncaci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day16_fibonacci
+{
+    class Program
+    {
+        static int Fibo(int num)
+        {
+            if (num == 0) return 0;
+            if (num == 1) return 1;
+            return Fibo(num - 1) + Fibo(num - 2);
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the length of the fibonacci series: ");
+            int fibonacci = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < fibonacci; i++)
+            {
+                Console.Write("{0} ", Fibo(i));
+            }
+        }
+
+    }
+}
---
+++ 26-july-2021/string_reversal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day16_stringReversal
+{
+    class Program
+    {
+        static string ReversalString(string sentence)
+        {
+            if (sentence.Length > 0)
+            {
+                return sentence[sentence.Length - 1] + ReversalString(sentence.Substring(0, sentence.Length - 1));
+            }
+            else
+            {
+                return sentence;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a sentence for reverse string: ");
+            var sentence = Console.ReadLine();
+            var reverseString = ReversalString(sentence);
+            Console.WriteLine(reverseString);
+        }
+    }
+}
