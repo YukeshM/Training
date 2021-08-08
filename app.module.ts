cd089a78dc6d14a401f673acd328d23e1e8bd6f6
Yukesh.M <76556005+YukeshM@users.noreply.github.com>
2021-08-08 19:36:29 +0530

    aug_6_2

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
